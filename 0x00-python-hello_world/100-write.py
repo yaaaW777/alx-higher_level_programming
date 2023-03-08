7e7763a10451d69f6960a99ba637a369a937d113
yaaaW777 <asanteantwi@gmail.com>
2023-03-07 17:14:28 -0800

    100 write

--- 0x00-python-hello_world/100-write.py
+++ 0x00-python-hello_world/100-write.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 import sys
-sys.stderr.write('and the piece of art is useful - DoraKorpa, 2015-10-19\n')
+sys.stderr.write("and the piece of art is useful - DoraKorpa, 2015-10-19\n")
 sys.exit(1)
