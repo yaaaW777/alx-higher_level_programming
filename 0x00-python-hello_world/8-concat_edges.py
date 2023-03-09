ee40ab39a3d10add5d4511f7fa77ac1374b73ce7
yaaaW777 <asanteantwi@gmail.com>
2023-03-09 10:13:44 -0800

    task 8

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -3,4 +3,3 @@ str = "Python is an interpreted, interactive, object-oriented programing\
 language that contains remarkable power with very clear syntax"
 str = str[39:67] + str[107:112] + str[:6]
 print(str)
-
