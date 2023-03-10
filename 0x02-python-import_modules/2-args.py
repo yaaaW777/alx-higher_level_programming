209a00d66734ec9bbbca5889be706c879a850b1b
yaaaW777 <asanteantwi@gmail.com>
2023-03-10 09:46:47 -0800

    task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,3 +7,13 @@ if __name__ == "__main__":
     size = len(arg) - 1
 
     if size > 1:
+         print("{} arguments:".format(size))
+        for i in range(1, size + 1):
+            print("{}: {}".format(i, arg[i]))
+
+    elif size == 0:
+        print("{} arguments.".format(size))
+
+    else:
+        print("{} argument:".format(size))
+        print("{}: {}".format(size, arg[1]))
