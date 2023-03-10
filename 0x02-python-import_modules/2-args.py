b259fb7d85116f4ddff63a5c39a6f856fd5b53a7
yaaaW777 <asanteantwi@gmail.com>
2023-03-10 09:49:58 -0800

    clear

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     size = len(arg) - 1
 
     if size > 1:
-         print("{} arguments:".format(size))
+        print("{} arguments:".format(size))
         for i in range(1, size + 1):
             print("{}: {}".format(i, arg[i]))
 
