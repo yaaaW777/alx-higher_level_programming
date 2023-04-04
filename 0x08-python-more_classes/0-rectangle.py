20f9f1996af1755f67336059f073c1fb5cc531dd
yaaaW777 <asanteantwi@gmail.com>
2023-04-04 02:50:36 -0700

    task 0

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Defining an empty class of a rectangle"""
+
+
+class Rectangle:
+    """An empty class epresent rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+a file for 0x08 python more classes and objects is in this readme
